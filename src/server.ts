0aeec4d480d06dfd89e237fe17cc32eb98530e2e
Mikaleb <mikaleb@live.fr>
2021-05-16 18:09:31 +0200

    chore(heroku): setup package and server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { checkDepartments } from '~/chronobot';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const CHECK_INTERVAL_SEC = Number(process.env.CHECK_INTERVAL_SEC) || 60; // check every X seconds
 const DEPARTMENTS_TO_CHECK = process.env.DEPARTMENTS_TO_CHECK!.split(',').map(Number);
